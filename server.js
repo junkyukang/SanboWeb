3f3cc05af82ce52deb8bc7dc1763db22c09f8f11
seong-bin-yun <tbvjaos3634@gmail.com>
2022-03-16 22:23:52 +0900

    글쓰기 폼 시도, 작성하기 누르면 작성페이지로 가는 느낌으로 만듦, db 적용하다 어려움을 격는중(각주처리)

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const bodyParser= require('body-parser');
 const port = 5000;
+//const db = require('./lib/db.js');
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}));
@@ -16,3 +17,58 @@ app.get('/', function(요청, 응답){
     응답.render('index.ejs'
     )
 })
+app.get('/page/create', function(요청, 응답){
+    응답.render('create.ejs'
+    )
+})
+/*app.post('/page/create_process', function(request, response){
+    var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      db.query(`
+        INSERT INTO topic (title, description, created, author_id) 
+          VALUES(?, ?, NOW(), ?)`,
+        [post.title, post.description, post.author], 
+        function(error, result){
+          if(error){
+            throw error;
+          }
+          response.writeHead(302, {Location: `/page/${result.insertId}`});
+          response.end();
+        }
+      )
+  });
+})
+    
+    
+    var post = request.body;
+    var title = post.title;
+    var description = post.description;
+    fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+      response.redirect(`/page/${title}`);
+    });
+  });
+
+
+  var body = '';
+  request.on('data', function(data){
+      body = body + data;
+  });
+  request.on('end', function(){
+      var post = qs.parse(body);
+      db.query(`
+        INSERT INTO topic (title, description, created, author_id) 
+          VALUES(?, ?, NOW(), ?)`,
+        [post.title, post.description, post.author], 
+        function(error, result){
+          if(error){
+            throw error;
+          }
+          response.writeHead(302, {Location: `/page/${result.insertId}`});
+          response.end();
+        }
+      )
+  });*/
