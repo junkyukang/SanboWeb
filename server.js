5d81cee2caed68f6b8ed5d316d0a4cba0b0ae2e6
junkyukang <jkkang130@naver.com>
2022-03-19 21:32:19 +0900

    aa

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 const bodyParser= require('body-parser');
-const port = 5000;
+const port = 80;
 
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended : true}));
